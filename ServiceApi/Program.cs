3205b09ccc71696605d8a68f5dce7b0f7971764a
dianaryzhenkova1@gmail.com <dianaryzhenkova1@gmail.com>
2026-04-24 00:58:09 +0400

    Выполнение лабораторной 2

--- ServiceApi/Program.cs
+++ ServiceApi/Program.cs
@@ -6,15 +6,10 @@ builder.AddServiceDefaults();
 builder.AddRedisDistributedCache("RedisCache");
 
 builder.Services.AddScoped<IEmployeeGeneratorService, EmployeeGeneratorService>();
-builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
-{
-    policy.WithOrigins("https://localhost:5127", "http://localhost:5127", "https://localhost:7282")
-          .WithMethods("GET")
-          .AllowAnyHeader();
-}));
+
 
 var app = builder.Build();
 app.MapDefaultEndpoints();
 app.MapGet("/employee", (IEmployeeGeneratorService service, int id) => service.ProcessEmployee(id));
-app.UseCors();
+
 app.Run();
